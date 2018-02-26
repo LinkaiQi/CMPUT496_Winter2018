fef908fedc5a227f786bc8849ad0b9e2c76cd47f
301githublinkai <linkai@ualberta.ca>
2018-02-26 12:37:49 -0700

    Implement Go endgame solver engine

--- endgame_solver/Go2/Go2.py
+++ endgame_solver/Go2/Go2.py
@@ -2,13 +2,17 @@
 # Set the path to your python3 above
 
 # Set up relative path for util; sys.path[0] is directory of current program
-import os, sys
+import os, sys, signal
 utilpath = sys.path[0] + "/../util/"
 sys.path.append(utilpath)
 
 from gtp_connection_go2 import GtpConnectionGo2
 from board_util import GoBoardUtil
 from simple_board import SimpleGoBoard
+
+# Register an handler for the timeout
+def handler(signum, frame):
+    raise Exception("end of time")
 
 class Go2():
     def __init__(self):
@@ -25,9 +29,80 @@ class Go2():
         """
         self.name = "Go2"
         self.version = 0.1
+        self.timelimit = 1
 
-    def get_move(self,board, color):
+    def get_move(self, board, color):
         return GoBoardUtil.generate_random_move(board,color,True)
+
+    def solve(self, board, to_play):
+        state = board.copy()
+        # to_play = state.current_player
+        # Initialize proof tree and run negamax search starts with current player
+        proof_tree = []
+        # Set timer
+        signal.signal(signal.SIGALRM, handler)
+
+        signal.alarm(self.timelimit)
+        try:
+            result = self.negamaxBoolean(state, proof_tree)
+        except Exception as e:
+            # print(e)
+            result = None
+        signal.alarm(0)
+
+        proof_tree.reverse()
+        # Convert result to current player
+        if result == None:
+            return 'unknown', None
+        elif result:
+            if proof_tree[0] == None:
+                proof_tree[0] = 'pass'
+            return GoBoardUtil.int_to_color(to_play), proof_tree[0]
+        else:
+            return GoBoardUtil.int_to_color(GoBoardUtil.opponent(to_play)), None
+        # elif result:
+        #     # convert 'point' to 'move'
+        #     coord = board._point_to_coord(proof_tree[0])
+        #     first_move = GoBoardUtil.format_point(coord)
+        #     return GoBoardUtil.int_to_color(to_play), first_move
+        # else:
+        #     return GoBoardUtil.int_to_color(GoBoardUtil.opponent(to_play)), None
+
+    def get_legal_moves(self, state):
+        legal_moves = []
+        for m in state.get_empty_points():
+            if state.check_legal(m, state.current_player) \
+            and not state.is_eye(m, state.current_player):
+                legal_moves.append(m)
+        if len(legal_moves) == 0:
+            legal_moves = [None]
+        # print(state.current_player, legal_moves)
+        return legal_moves
+
+    def negamaxBoolean(self, state, proof_tree):
+        if state.end_of_game():
+            winner, score = state.score(self.komi)
+            # print('-----------', winner, '-----------')
+            if winner == state.current_player:
+                return True
+            else:
+                return False
+        for m in self.get_legal_moves(state):
+            #---------------
+            # if m:
+            #     coord = state._point_to_coord(m)
+            #     print(GoBoardUtil.format_point(coord))
+            # else:
+            #     print('pass')
+            #---------------
+            state.move(m, state.current_player)
+            success = not self.negamaxBoolean(state, proof_tree)
+            state.undo_move()
+            if success:
+                proof_tree.append(m)
+                return True
+        return False
+
 
 
 def run():
--- endgame_solver/Go2/gtp_connection_go2.py
+++ endgame_solver/Go2/gtp_connection_go2.py
@@ -31,6 +31,64 @@ class GtpConnectionGo2(gtp_connection.GtpConnection):
         self.commands["go_safe"] = self.safety_cmd
         self.argmap["go_safe"] = (1, 'Usage: go_safe {w,b}')
 
+        # timelimit seconds cmd
+        self.commands["timelimit"] = self.timelimit_cmd
+        self.argmap["timelimit"] = (1, 'Usage: timelimit seconds')
+        # solve cmd
+        self.commands["solve"] = self.solve_cmd
+
+    def timelimit_cmd(self, args):
+        """
+        Set the maximum time to use for following 'genmove' or 'solve' cmd
+
+        Arguments
+        ---------
+        args[0] : int range(1<= seconds <= 100)
+            timelimit seconds
+        """
+        self.go_engine.timelimit = int(args[0])
+        self.respond()
+
+    # convert 'point' to 'board_move'
+    def _point_to_move(self, point):
+        if point == 'pass':
+            return 'pass'
+        elif point:
+            coord = self.board._point_to_coord(point)
+            return GoBoardUtil.format_point(coord)
+        return None
+
+    def solve_cmd(self, args):
+        winner, point = self.go_engine.solve(self.board, self.board.current_player)
+        board_move = self._point_to_move(point)
+        # show result in stdout
+        if board_move:
+            self.respond('{} {}'.format(winner, board_move))
+        else:
+            self.respond(winner)
+
+    def genmove_cmd(self, args):
+        board_color = args[0].lower()
+        color = GoBoardUtil.color_to_int(board_color)
+        # Try to use the solver to solve the game perfectly
+        winner, point = self.go_engine.solve(self.board, color)
+        if winner == 'unknown' or winner != board_color:
+            # If the solver cannot solve the game in 'timelimit' seconds, or toPlay is losing
+            # then play the same move as the original Go2
+            super().genmove_cmd(args)
+        else:
+            if point == None or point == 'pass':
+                self.respond("pass")
+                return
+            self.board.move(point, color)
+            self.debug_msg("Move: {}\nBoard: \n{}\n".format(point, str(self.board.get_twoD_board())))
+            board_move = self._point_to_move(point)
+            self.respond(board_move)
+
+    # def genmove_cmd(self, args):
+    #     super().genmove_cmd(args)
+
+
     def safety_cmd(self, args):
         try:
             color= GoBoardUtil.color_to_int(args[0].lower())
